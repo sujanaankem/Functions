0d3c04bb2a8df995614ecd5285ef94941416534e
sujanaankem <ankemsujana@gmail.com>
2026-09-01 23:56:57 +0530

    Array functions

---
+++ ArrowFunctions/ArrowFunctions.js
@@ -0,0 +1,9 @@
+// Arrow function to display a message
+const showMessage = () => {
+  document.getElementById("output").textContent = "Hello, Sujana! This is an arrow function in action.";
+};
+
+// Another arrow function to add numbers
+const add = (a, b) => a + b;
+
+console.log(add(5, 7)); // Output: 12
