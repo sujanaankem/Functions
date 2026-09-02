6a946fbb5a26dc7c47dcea44792805028ed011a6
sujanaankem <ankemsujana@gmail.com>
2026-09-02 20:28:44 +0530

    Arguments

---
+++ Arguments/Arguments.js
@@ -0,0 +1,6 @@
+// Function with parameters
+function calculateSum(a, b) {
+  let sum = a + b;
+  document.getElementById("result").textContent = 
+    "The sum of " + a + " and " + b + " is: " + sum;
+}
